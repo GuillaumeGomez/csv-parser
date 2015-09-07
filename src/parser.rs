3810d2c209695697122428ccca9b11e523d847fa
Guillaume Gomez <guillaume1.gomez@gmail.com>
2015-09-08 00:01:22 +0200

    ICE

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,13 @@
-use nom::{IResult, digit, multispace};
+use nom::{IResult, not_line_ending, line_ending};
 
-struct CSV {
-	keys: Vec<&str>,
-	values: Vec<Vec<&str>>,
+fn csv_line(input: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
+	terminated!(input, separated_list!(filter!(apply!(check_characters, b"\n\r,")), not_line_ending), line_ending)
 }
 
-terminated!(separated_list!(filter!(check_characters(b"\n\r,")), not_line_ending), line_ending, a);
-
-fn check_characters(characters: &[u8], data: &[u8]) -> bool {
+fn check_characters(data: &[u8], characters: &[u8]) -> bool {
 	for i in 0..data.len() {
 		for c in characters {
-			if data[i] == c {
+			if data[i] == *c {
 				return false;
 			}
 		}
@@ -18,13 +15,9 @@ fn check_characters(characters: &[u8], data: &[u8]) -> bool {
 	true
 }
 
-/*fn fill_keys(input: &[u8]) -> IResult<&[u8], Vec<&str>> {
-
-}*/
-
 #[test]
 fn check_file() {
 	let f = b"nom,age\ncarles,30\nlaure,28";
 
-	separated_list!(f);
+	csv_line(f);
 }
